6263ddf9a5ddb679a813fa32af7e06f5899bfa5e
Nicolas Alvarez <alvarez99@gmail.com>
2020-05-03 17:49:40 -0500

    Update Validando contenedores.py

--- Validando contenedores.py
+++ Validando contenedores.py
@@ -12,8 +12,3 @@ driver.get("https://www.w3schools.com/html/default.asp")
 time.sleep(5)
 all_cookies = driver.get_cookies()
 print(all_cookies)
-driver.quit()
-from selenium import webdriver
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-import time
-
