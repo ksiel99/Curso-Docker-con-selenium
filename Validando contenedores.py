e9b39b014a9fcb015c09e28350ebad69f537f68d
Nicolas Alvarez <alvarez99@gmail.com>
2020-05-03 17:48:39 -0500

    Add files via upload

---
+++ Validando contenedores.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys 
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+import time
+
+driver = webdriver.Remote(
+'http://localhost:4444/wd/hub',
+   desired_capabilities=webdriver.DesiredCapabilities.CHROME
+)
+
+driver.get("https://www.w3schools.com/html/default.asp")
+time.sleep(5)
+all_cookies = driver.get_cookies()
+print(all_cookies)
+driver.quit()
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+import time
+
